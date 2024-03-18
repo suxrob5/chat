fc6880a06105151ae26d510ab38d178e8b7030c3
Suxrob <orifovsuxrov@gmail.com>
2024-03-18 22:40:04 +0500

    buutoon

--- src/components/SigIn.js
+++ src/components/SigIn.js
@@ -1,10 +1,19 @@
-const SignIn = ({auth,firebase}) => {
+const SignIn = ({ auth, firebase }) => {
   const signInWithGoogle = () => {
     const provider = new firebase.auth.GoogleAuthProvider();
     auth.signInWithPopup(provider);
   };
 
-  return <button onClick={signInWithGoogle}>Sign In with Google</button>;
+  return (
+    <div>
+      <button
+        onClick={signInWithGoogle}
+        className="font-[600] text-3xl w-[300px] rounded-lg hover:bg-opacity-90 bg-blue-500 hover:shadow-xl active:shadow-2xl"
+      >
+        Sign In with Google
+      </button>
+    </div>
+  );
 };
 
 export default SignIn;
